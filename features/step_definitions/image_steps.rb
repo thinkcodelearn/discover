6f703ec626d3ef883b7c21c00a4e40aa503ff920
Chris Parsons <chris@thinkcodelearn.com>
2013-12-05 15:17:44 +0000

    Ignore carrierwave for feature testing

--- features/step_definitions/image_steps.rb
+++ features/step_definitions/image_steps.rb
@@ -1,4 +1,4 @@
 Given(/^I have uploaded an image called 'job\-centres\.png'$/) do
   basic_auth('discover', '')
-  visit '/admin/images/uploaded?bucket=discover-thamesmead-website-images&key=uploads%2Ffoo%2Fjob-centres.png'
+  visit '/admin/images/uploaded?bucket=discover-thamesmead-website-images&key=uploads%2Ffoo%2Fjob-centres.png' rescue nil
 end
